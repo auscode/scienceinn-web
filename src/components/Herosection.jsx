b372b8a99213ef3d4efb400b4ca86d725398b3ff
hm6091999 <hm6091999@gmail.com>
2024-07-04 05:00:17 +0000

    hero section responsive ness

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -1,18 +1,19 @@
-import heroImg from '../assets/hero_section.webp'
+import heroImg from '../assets/hero_section.webp';
 
 export default function Herosection() {
     return (
-        <>
-            <div >
-                <div className='relative h-200px'>
-                    <img src={heroImg} alt='blackhole-image-from-behence ' className='' />
-                    <div className='absolute lg:top-[15em] lg:left-[6em]'>
-                        <div className='text-8xl text-white font-bold font-sans'>ScienceInn</div>
-                        <div className='text-3xl text-grey-500 font-bold font-sans'>Born To learn</div>
-                        {/* <button></button> */}
+        <div>
+            <div className='relative h-[200px] md:h-[600px] lg:h-[800px]'>
+                <img src={heroImg} alt='blackhole-image-from-behence' className='w-full h-full object-cover' />
+                <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center'>
+                    <div className='text-4xl md:text-6xl lg:text-8xl text-white font-bold font-sans'>
+                        ScienceInn
+                    </div>
+                    <div className='text-xl md:text-2xl lg:text-3xl text-gray-500 font-bold font-sans mt-4'>
+                        Born To Learn
                     </div>
                 </div>
             </div>
-        </>
-    )
+        </div>
+    );
 }
