483881ee47a697e9117888782d6fed8283679305
hm6091999 <hm6091999@gmail.com>
2024-07-04 11:41:45 +0000

    gallary responsiveness

--- src/components/pages/Gallery.jsx
+++ src/components/pages/Gallery.jsx
@@ -14,6 +14,7 @@ const Gallery = () => {
         }
         return description;
     };
+
     const fetchImages = async () => {
         try {
             const response = await fetch(`https://images-api.nasa.gov/search?q=${encodeURIComponent(query)}&media_type=image`);
@@ -21,7 +22,6 @@ const Gallery = () => {
                 throw new Error('Failed to fetch images');
             }
             const data = await response.json();
-            console.log(data);
             const items = data.collection.items;
             const imageList = items.map(item => ({
                 url: item.links[0].href,
@@ -44,7 +44,6 @@ const Gallery = () => {
         fetchImages();
     };
 
-
     const formatDate = (dateString) => {
         const date = new Date(dateString);
         return date.toLocaleDateString('en-US', {
@@ -58,8 +57,8 @@ const Gallery = () => {
     };
 
     return (
-        <div className='bg-black text-white'>
-            <div className='text-4xl font-bold text-center p-20 pb-10'>Gallery</div>
+        <div className='bg-black text-white min-h-screen'>
+            <div className='text-4xl font-bold text-center p-10 sm:p-20 pb-5 sm:pb-10'>Gallery</div>
             <div className='text-center mb-4'>
                 <input
                     type="text"
@@ -75,23 +74,24 @@ const Gallery = () => {
                     Search
                 </button>
             </div>
-            <div className='flex justify-center itms-center'>
-                <div className='font-bold flex justify-center items-center text-6xl p-4 w-1/2'>
-                    Location of the Iss is tracked Here
+            <div className='flex flex-col lg:flex-row justify-center items-center p-4'>
+                <div className='font-bold text-3xl sm:text-6xl text-center lg:text-left p-4 lg:w-1/2'>
+                    Location of the ISS is tracked Here
                 </div>
-                <div className='m-4 border-2 rounded-xl w-1/2'>
+                <div className='m-4 border-2 rounded-xl w-full lg:w-1/2 border-2'>
                     <ISSGlobe />
                 </div>
-
             </div>
             <div className='gallery-data m-4'>
                 {error && <div className='text-red-500 text-center'>{`Error: ${error}`}</div>}
-                <div className='grid grid-cols-3 gap-4'>
+                <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
                     {images.map((image, index) => (
-                        <div key={index} className='border rounded-lg overflow-hidden p-2'>
-                            <img src={image.url} alt={image.title} className='object-cover rounded-lg' />
+                        <div key={index} className='grid gap-4 border-2 p-2 border-white rounded-lg'>
+                            <div className='h-48 w-full'>
+                                <img src={image.url} alt={image.title} className='object-contain w-full h-full rounded-lg' />
+                            </div>
                             <div className='font-bold text-xl'>{image.title}</div>
-                            <div className='font-semibold text-gray-300'>{truncateDescription(image.description, 30)}</div>
+                            <div className='font-semibold text-gray-300'>{truncateDescription(image.description, 12)}</div>
                             {image.location && <div className=''>Location: {image.location}</div>}
                             <div className=''>{formatDate(image.date)}</div>
                         </div>
